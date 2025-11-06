a494162937424716ddb8ce1c407160695f55b3e4
saipavan kumar <saipavan345>
2025-11-06 12:38:11 +0530

    Describe changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import "swiper/css";
 import Home from "./Home/Home.jsx"
 //import Home from "./Home/Home.jsx";
@@ -12,9 +12,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 // fonts and icons
-import "././assets/css/icofont.min.css";
-import "././assets/css/animate.css";
-import "././assets/css/style.min.css";
+// import "././assets/css/icofont.min.css";
+// import "././assets/css/animate.css";
+// import "././assets/css/style.min.css";
 
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
